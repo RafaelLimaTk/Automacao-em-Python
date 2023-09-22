96d1785e60281287b6f49c1ff69ed8d0482a051a
RafaelLimaTk <rafamano123.rl@gmail.com>
2023-09-22 09:32:33 -0300

    Criação da interface, compactação em zip/download

--- automacao_relatorios_professores.py
+++ automacao_relatorios_professores.py
@@ -1,7 +1,13 @@
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.chart import BarChart, Reference
+from tkinter import filedialog
+from zipfile import ZipFile
+
 import pandas as pd
+import tkinter as tk
+import os
+import shutil
 
 class ProfessorEvaluation:
     def __init__(self, professor_name, question, evaluation_data, evaluation_data_value, weighted_average):
@@ -11,7 +17,7 @@ class ProfessorEvaluation:
         self.evaluation_data_value = evaluation_data_value
         self.weighted_average = weighted_average
 
-def create_excel_report_for_professor(professor_evaluation_list, output_file_path):
+def create_excel_report_for_professor(professor_evaluation_list, output_file_path, folder_path):
     wb = Workbook()
     ws = wb.active
 
@@ -19,10 +25,10 @@ def create_excel_report_for_professor(professor_evaluation_list, output_file_pat
         if idx > 0:
             ws = wb.create_sheet(title=f"Q{idx+1}")
 
-        ws.append([f"Question: {prof_eval.question}"])
+        ws.append([f"QUestão: {prof_eval.question}"])
         ws.append([f"Professor: {prof_eval.professor_name}"])
         ws.append([])
-        ws.append(['Evaluation Characteristics', 'Percentage'])
+        ws.append(['Características de Avaliação', 'Porcentagem'])
         ws.append(['Média ponderada', 'Média Ponderada ({})'.format(prof_eval.weighted_average)])
 
         for eval_char, percentage in prof_eval.evaluation_data.items():
@@ -46,40 +52,91 @@ def create_excel_report_for_professor(professor_evaluation_list, output_file_pat
 
         ws.add_chart(chart, f"H{1 + len(prof_eval.evaluation_data)}")
 
-    wb.save(output_file_path)
+    save_path = os.path.join(folder_path, output_file_path)
+    wb.save(save_path)
 
-input_file_path = "Avaliação Docente 2022.2 Aluno - Odontologia - 2 PERÍODO.xlsx"
-df = pd.read_excel(input_file_path, header=None)
-question_rows = df[df.iloc[:, 0].str.contains("Q[0-9]+", na=False, regex=True)].index.tolist()
-all_professors_data = {}
+def process_excel_file(input_file_path, folder_path):
+    df = pd.read_excel(input_file_path, header=None)
+    question_rows = df[df.iloc[:, 0].str.contains("Q[0-9]+", na=False, regex=True)].index.tolist()
+    all_professors_data = {}
 
-for i in range(len(question_rows) - 1):
-    start_row = question_rows[i]
-    end_row = question_rows[i + 1] if i < len(question_rows) - 1 else len(df)
+    for i in range(len(question_rows) - 1):
+        start_row = question_rows[i]
+        end_row = question_rows[i + 1] if i < len(question_rows) - 1 else len(df)
 
-    question = df.iloc[start_row, 0]
+        question = df.iloc[start_row, 0]
 
-    evaluation_characteristics = df.iloc[start_row + 1, 1::2].dropna()
-    evaluation_characteristics_value = df.iloc[start_row + 2, 1::2].dropna()
-    evaluation_characteristics_value[evaluation_characteristics_value.index <= 11] *= 100
+        evaluation_characteristics = df.iloc[start_row + 1, 1::2].dropna()
+        evaluation_characteristics_value = df.iloc[start_row + 2, 1::2].dropna()
+        evaluation_characteristics_value[evaluation_characteristics_value.index <= 11] *= 100
 
-    professor_data = df.iloc[start_row + 2 : end_row].dropna(subset=[df.columns[0]])
-    professor_data = professor_data.iloc[:, ::2]
+        professor_data = df.iloc[start_row + 2 : end_row].dropna(subset=[df.columns[0]])
+        professor_data = professor_data.iloc[:, ::2]
+        
+        eval_chars_list = evaluation_characteristics.tolist()
+        eval_value_list = evaluation_characteristics_value.tolist()
+        for index, row in professor_data.iterrows():
+            professor_name = row.iloc[0]
+            evaluation_data = {eval_chars_list[i]: row.iloc[i+1] for i in range(len(eval_chars_list))}
+            evaluation_data_value = {eval_value_list[i]: row.iloc[i+1] for i in range(len(eval_value_list))}
+            weighted_average = row.iloc[-1]      
+            prof_eval = ProfessorEvaluation(professor_name, question, evaluation_data, evaluation_data_value, weighted_average)
+            
+            if professor_name not in all_professors_data:
+                all_professors_data[professor_name] = []
+            
+            all_professors_data[professor_name].append(prof_eval)
+
+    for professor_name, professor_evaluation_list in all_professors_data.items():
+        output_file_path = f"Avaliação_{professor_name.replace('/', '_').replace(' ', '_')}.xlsx"
+        create_excel_report_for_professor(professor_evaluation_list, output_file_path, folder_path)
+
+def download_zip_file(folder_name):
+    save_path = filedialog.asksaveasfilename(initialfile=f"{folder_name}.zip", defaultextension=".zip", filetypes=[("ZIP files", "*.zip")])
+    if not save_path:
+        return
+    shutil.copy(zip_file_path, save_path)
+
+def select_and_process_files():
+    file_paths = filedialog.askopenfilenames(filetypes=[("Excel files", "*.xlsx")])
+    if not file_paths:
+        return
     
-    eval_chars_list = evaluation_characteristics.tolist()
-    eval_value_list = evaluation_characteristics_value.tolist()
-    for index, row in professor_data.iterrows():
-        professor_name = row.iloc[0]
-        evaluation_data = {eval_chars_list[i]: row.iloc[i+1] for i in range(len(eval_chars_list))}
-        evaluation_data_value = {eval_value_list[i]: row.iloc[i+1] for i in range(len(eval_value_list))}
-        weighted_average = row.iloc[-1]      
-        prof_eval = ProfessorEvaluation(professor_name, question, evaluation_data, evaluation_data_value, weighted_average)
-        
-        if professor_name not in all_professors_data:
-            all_professors_data[professor_name] = []
-        
-        all_professors_data[professor_name].append(prof_eval)
+    os.makedirs("Avaliações", exist_ok=True)
+    
+    for file_path in file_paths:
+        if not file_path.lower().endswith('.xlsx'):
+            print(f"O arquivo {file_path} não é um arquivo .xlsx válido.")
+            continue
 
-for professor_name, professor_evaluation_list in all_professors_data.items():
-    output_file_path = f"Avaliação_{professor_name.replace('/', '_').replace(' ', '_')}.xlsx"
-    create_excel_report_for_professor(professor_evaluation_list, output_file_path)
+        # Criar uma pasta para cada arquivo
+    for file_path in file_paths:
+        folder_name = os.path.splitext(os.path.basename(file_path))[0]
+        folder_path = os.path.join("Avaliações", folder_name)
+        os.makedirs(folder_path, exist_ok=True)
+
+        # Processar o arquivo Excel e salvar na pasta
+        process_excel_file(file_path, folder_path)
+
+    # Comprimir a pasta em um arquivo ZIP
+    global zip_file_path  # Tornando global para acessar na função download_zip_file
+    zip_file_path = os.path.join("Avaliações", f"{folder_name}.zip")
+    with ZipFile(zip_file_path, 'w') as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), folder_path))
+    
+    download_button.config(state=tk.NORMAL, command=lambda: download_zip_file(folder_name))
+    return folder_name
+
+# Criar a interface Tkinter
+root = tk.Tk()
+root.title("Processador de Arquivos Excel")
+
+button = tk.Button(root, text="Selecionar Arquivos", command=select_and_process_files)
+button.pack()
+
+download_button = tk.Button(root, text="Baixar ZIP", command=download_zip_file, state=tk.DISABLED)
+download_button.pack()
+
+root.mainloop()
