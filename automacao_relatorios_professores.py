9c27202c0b07ce2d0ffedc0ed5226d6bd314a7bd
RafaelLimaTk <rafamano123.rl@gmail.com>
2023-10-19 11:38:29 -0300

    salvando os arquivos já convertidos em pdf

--- automacao_relatorios_professores.py
+++ automacao_relatorios_professores.py
@@ -15,6 +15,8 @@ import tkinter as tk
 import os
 import shutil
 import re
+import win32com.client
+import gc
 
 zip_file_paths = []
 
@@ -120,12 +122,53 @@ def create_excel_report_for_professor(professor_evaluation_list, output_file_pat
 
         df = pd.read_excel(save_path)
 
+        save_path_absolute = os.path.abspath(save_path)
+        pdf_output_file_path = save_path_absolute.replace('.xlsx', '.pdf')
+        pdf_output_file_path_absolute = os.path.abspath(pdf_output_file_path)
+
+        conversions = [(save_path_absolute, pdf_output_file_path_absolute)]
+
+        excel = win32com.client.Dispatch("Excel.Application")
+        try:
+            for input_file, output_file in conversions:
+                excel_to_pdf(excel, input_file, output_file)
+        finally:
+            excel.Quit()
+
         return True
     
     except PermissionError:
         log_error(f"Permissão negada ao tentar salvar o arquivo {output_file_path}.")
     except Exception as e:
         log_error(f"Erro ao salvar o arquivo {output_file_path}: {e}")
+
+def excel_to_pdf(excel, input_file, output_file):
+    if not os.path.exists(input_file):
+        print(f"O arquivo {input_file} não existe.")
+        return
+
+    workbook = None
+    try:
+        excel.Visible = False
+        excel.DisplayAlerts = False
+        excel.ScreenUpdating = False
+
+        workbook = excel.Workbooks.Open(input_file)
+        
+        ws_index_list = list(range(1, workbook.Worksheets.Count + 1))
+        workbook.Worksheets(ws_index_list).Select()
+
+        workbook.ActiveSheet.ExportAsFixedFormat(0, output_file)
+        
+        workbook.Close(True)
+        
+        print("Conversão bem-sucedida.")
+        
+    except Exception as e:
+        print(f"Erro na conversão: {e}")
+    finally:
+        if workbook:
+            workbook.Close(True)
 
 def process_excel_file(input_file_path, folder_path):
     df = read_excel_file(input_file_path)
